0d45c6fb47c7796f3d47cdb4f6139ded6e990472
Your Name <you@example.com>
2026-08-21 22:44:55 +0530

    Q3: sum of two numbers

---
+++ unit1_algorithms/q3_sum.py
@@ -0,0 +1,9 @@
+# CS107PSP Q3 — Sum of Two Numbers
+# Name: Jani Janvi
+# Roll No: 2626297
+# Branch: MCA Data Science
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+total = a + b
+print("Sum =", total)
