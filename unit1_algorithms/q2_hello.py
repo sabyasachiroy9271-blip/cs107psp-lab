d7067ecd2fda621e25997706e41e1c73163a882e
Your Name <you@example.com>
2026-08-21 22:27:22 +0530

    Q3: sum of two numbers

---
+++ unit1_algorithms/q2_hello.py
@@ -0,0 +1,9 @@
+# CS107PSP Q2 — Hello World
+# Name: Sabyasachi roy
+# Roll No: 2626297
+# Branch: MCA Data Science
+
+print("Hello, World!")
+print("Name: Sabyasachi roy")
+print("Roll No: 2626296")
+print("Branch: MCA Data Science")
